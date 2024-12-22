48ae25ac695da315cda8107e7c450ceb610e8a20
Krishnendu Nandy <krishnendunandy6@gmail.com>
2024-12-22 22:50:15 +0530

    branch-15 #comment gallery page banner section, homepage btn design change

--- website/src/App.js
+++ website/src/App.js
@@ -5,6 +5,7 @@ import './style/home.css';
 import './style/google-review-widget.css';
 import './style/review.css';
 import './style/our-service.css';
+import './style/work-gallery.css';
 import './style/gallery-carousel.css';
 import './style/custom-masonary.css';
 import './App.css';
--- website/src/component/custom-masonary.js
+++ website/src/component/custom-masonary.js
@@ -3,68 +3,68 @@ import React from "react";
 export default function CustomMasonary(){
     return(
         <div className="custom-masonary">
-            <div class="grid-layout">
-                <div class="grid-item grid-item-one span-2-by-2">
+            <div className="grid-layout">
+                <div className="grid-item grid-item-one span-2-by-2">
                     <div className="item">
                         <p className="item-title mb-0">Wedding</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-two">
+                <div className="grid-item grid-item-two">
                     <div className="item">
                         <p className="item-title mb-0">Rice Ceremony</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-three">
+                <div className="grid-item grid-item-three">
                     <div className="item">
                         <p className="item-title mb-0">Birthday</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-four span-2-by-2">
+                <div className="grid-item grid-item-four span-2-by-2">
                     <div className="item">
                         <p className="item-title mb-0">Corporate Event</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-five span-2-by-1">
+                <div className="grid-item grid-item-five span-2-by-1">
                     <div className="item">
                         <p className="item-title mb-0">Get Together</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-eleven">
+                <div className="grid-item grid-item-eleven">
                     <div className="item">
                         <p className="item-title mb-0">Sraddha Anusthan</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-ten">
+                <div className="grid-item grid-item-ten">
                     <div className="item">
                         <p className="item-title mb-0">Engagement</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-eight">
+                <div className="grid-item grid-item-eight">
                     <div className="item">
                         <p className="item-title mb-0">Baby Shower (Saadh)</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-nine span-2-by-2">
+                <div className="grid-item grid-item-nine span-2-by-2">
                     <div className="item">
                         <p className="item-title mb-0">Upanayan (Poite)</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-twelve">
+                <div className="grid-item grid-item-twelve">
                     <div className="item">
                         <p className="item-title mb-0">Anniversary</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-seven span-1-by-2">
+                <div className="grid-item grid-item-seven span-1-by-2">
                     <div className="item">
                         <p className="item-title mb-0">Griha Pravesh</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-six">
+                <div className="grid-item grid-item-six">
                     <div className="item">
                         <p className="item-title mb-0">Festivals</p>
                     </div>
                 </div>
-                <div class="grid-item grid-item-thirteen">
+                <div className="grid-item grid-item-thirteen">
                     <div className="item">
                         <p className="item-title mb-0">Other Events</p>
                     </div>
