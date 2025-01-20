a002bf65f602473544e2ad616e289e66f2e2a574
KRISHNENDU NANDY <krishnendunandy6@gmail.com>
2025-01-21 04:08:25 +0530

    branch-29 #comment api response fix 3

--- backend/controller/packageDataApi.js
+++ backend/controller/packageDataApi.js
@@ -1,32 +1,17 @@
 const foodPackageModel = require('../db_models/food_package_model');
 
-// exports.getFoodPackage = async (req,res) =>{
-//     await foodPackageModel.find({}).then(
-//         (package) => {
-//             if(package.length > 0){
-//                 res.status(200).json({success: true, message:"Food Packages fetch successfully!", data:package});
-//             }else{
-//                 res.status(500).json({success: false, message:"No Package added yet"});
-//             }
-//         }
-//     ).catch(
-//         (error)=> {
-//             res.status(500).body({success: false, message:error});
-//         }
-//     );
-// }
-exports.getFoodPackage = (req,res) => {
-    const food = foodModel.find({}).then(
-        (food) =>{
-            res.json([{message: "success"},{"data":food}]);
+exports.getFoodPackage = async (req,res) =>{
+    await foodPackageModel.find({}).then(
+        (package) => {
+            if(package.length > 0){
+                res.status(200).json({success: true, message:"Food Packages fetch successfully!", data:package});
+            }else{
+                res.status(500).json({success: false, message:"No Package added yet"});
+            }
         }
     ).catch(
-        (error) => {
-            response.status(500).json({
-                success: false,
-                message: "Internal server error",
-                error: error.message
-            })
+        (error)=> {
+            res.status(500).body({success: false, message:error});
         }
-    )
+    );
 }
